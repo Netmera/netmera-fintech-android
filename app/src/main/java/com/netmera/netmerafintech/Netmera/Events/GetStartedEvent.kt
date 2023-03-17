16cc31018fc1847429ccdeea20ff9dff5e29cc9a
yasirinitialcode <115461849+yasirinitialcode@users.noreply.github.com>
2023-03-17 11:07:31 +0300

    - Netmera integration has been done, events firing structure has been set up for first screen. (#5)
    * - Netmera integration was done, user update, get started and sign in events firing structure has been set up.
    
    * - Huawei libraries were removed.
    
    * - Unnecessary code lines were removed.

---
+++ app/src/main/java/com/netmera/netmerafintech/Netmera/Events/GetStartedEvent.kt
@@ -0,0 +1,20 @@
+package com.netmera.netmerafintech.Netmera.Events
+
+import com.google.gson.annotations.SerializedName
+import com.netmera.NetmeraEvent
+
+class GetStartedEvent : NetmeraEvent() {
+    @SerializedName("fg")
+    private var getStarted: Boolean? = null
+    override fun eventCode(): String {
+        return EVENT_CODE
+    }
+
+    fun setGetStarted(getStarted: Boolean?) {
+        this.getStarted = getStarted
+    }
+
+    companion object {
+        private const val EVENT_CODE = "radgk"
+    }
+}
